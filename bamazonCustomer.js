c8a0798866a430d5de0ba82d6f922c11a84b1ff3
PMack <prestonmack@gmail.com>
2018-11-04 11:15:47 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,62 +29,82 @@ function afterConnection() {
         for (var i = 0; i < data.length; i++) {
             console.log("ID: " + data[i].item_id + " || Product Name: " + data[i].product_name, + " || Department: " + data[i].department_name + " || Price: " + data[i].price + " || Stock: " + data[i].stock_quantity);
         }
-        start();
+        // start();
 
     });
 }
 
 // function which prompts the user for what action they should take
-function start() {
-    inquirer
-        .prompt({
-            name: "item_id",
-            type: "input",
-            message: "What is the ID of the product you want to buy?",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                } else {
-                    console.log('\nPlease enter a valid ID.');
-                    return false;
-                }
-            }
-        }, {
-                name: 'stock_quantity',
-                message: 'How many units would you like to buy?',
-                type: 'input',
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter a valid quantity.');
-                        return false;
-                    }
-                }
+// function start() {
+//     inquirer
+//         .prompt({
+//             name: "item_id",
+//             type: "input",
+//             message: "What is the ID of the product you want to buy?",
+//             validate: function (value) {
+//                 if (isNaN(value) === false) {
+//                     return true;
+//                 } else {
+//                     console.log('\nPlease enter a valid ID.');
+//                     return false;
+//                 }
+//             }
+//         }, {
+//                 name: 'stock_quantity',
+//                 message: 'How many units would you like to buy?',
+//                 type: 'input',
+//                 validate: function (value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     } else {
+//                         console.log('\nPlease enter a valid quantity.');
+//                         return false;
+//                     }
+//                 }
+            
 
-
-            }).then(function (answer) {
-                return new Promise(function (resolve, reject) {
-                    // query for all items in products table where the item_id is what was chosen
-                    connection.query("SELECT * FROM products WHERE item_id=?", answer.item_id, function (err, res) {
-                        if (err) reject(err);
-                        resolve(res);
-                    });
-
-                }).then(function (result) {
-                    // if there aren't enough of the item
-                    if (answer.stock_quantity > result[0].stock_quantity) {
-                        console.log ("Insufficient quantity!");
-                        // if there are enough
-                    } else {
-                        var object = {};
-                        // answer is the users responses to the prompts
-                        object.answer = answer;
-                        // result is the results of the query
-                        object.result = result;
-                        return object;
-                    }
-                })
-            })
-                
-
+//         }]).then(function(answer) {
+//             return new Promise(function(resolve, reject) {
+//                 // query for all items in products table where the item_id is what was chosen
+//                 connection.query("SELECT * FROM products WHERE item_id=?", answer.item_id, function(err, res) {
+//                     if (err) reject(err);
+//                     resolve(res);
+//                 });
+//             }).then(function(result) {
+//                 // if there aren't enough of the item
+//                 if (answer.number_of_units > result[0].stock_quantity) {
+//                     return "Insufficient quantity!";
+//                 // if there are enough
+//                 } else {
+//                     var object = {};
+//                     // answer is the users responses to the prompts
+//                     object.answer = answer;
+//                     // result is the results of the query
+//                     object.result = result;
+//                     return object;
+//                 }
+//             }).catch(function(err) {
+//                 console.log(err);
+//                 connection.end();
+//             }).then(function(object) {
+//                 // if there was sufficient quantity
+//                 if (object.answer) {
+//                     var newQuantity = object.result[0].stock_quantity - object.answer.number_of_units;
+//                     var product = object.answer.item_id;
+//                     var totalCost = (object.result[0].price * object.answer.number_of_units).toFixed(2);
+//                     // query that updates the quantity of the item
+//                     connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [newQuantity, product], function(err, res) {
+//                         if (err) reject(err);
+//                         console.log('Your total cost is $' + totalCost);
+//                         // destroy connection
+//                         connection.destroy();
+//                     });
+//                 } else {
+//                     console.log(object);
+//                     // end connection
+//                     connection.end();
+//                 }
+//             });
+//         });
+//     }
+    
